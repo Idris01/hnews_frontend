0052f65b56cbb16288c96a6c71d5652ac8d40878
idris01 <idrys01@gmail.com>
2023-08-29 22:04:21 +0100

    add news detail page

--- src/app/news/[news_type]/[news_id]/page.tsx
+++ src/app/news/[news_type]/[news_id]/page.tsx
@@ -1,34 +1,59 @@
 import React from 'react'
+import Link from 'next/link'
 import { BASE_URL } from '@/components/apiUrls'
+import { NewsType } from '@/components/types'
+
+const newMessage:NewsType = {
+		id:0, item_type:'story', title:"news", api_id:0, by:'idris', created_at:"20"
+	};
 
 export async function generateStaticParams(){
 	return [{news_id: '1'}, {news_type: '2'}]
 }
-async function getNews(params:any){
-	let status = 200
-	try {
-		 const resp = await fetch(`${BASE_URL}/news-api/story/${params.news_id}`)
-		status = resp.status;
-		const news = await resp.json()
-		return news;
-	}	
-	catch {
 
-		return { 
-			message: "An Error Has Occured",
-			status
-		}
+async function getNews(params:{news_id:number, news_type:string}){
+	const {news_id, news_type} = params
+	
+	const data = {
+		news:newMessage,
+		status:200,
+		message:""
 	}
+
+	try {
+		const url = `${BASE_URL}/news-api/${news_type}/${news_id}`
+		const resp = await fetch(url);
+		if (resp.status > 299) throw Error(`${resp.status}`);
+		data.news = await resp.json();
+	}
+	catch(errorStatus:any) {
+		data.message= "an Error has occured";
+		data.status = errorStatus;
+	}
+	return data;
 }
 
 export default async function NewsItem({params}:any) {
-	const news = await getNews(params.news_id)
-	const {message, status} = news;
+	const res = await getNews(params)
+	const {message, status, news} = res;
+	const { url, text, title} = news;
 
-	if (status > 299) return <div>{params.news_type}</div>;
+	if (status > 299) return <div>{message}</div>;
 	return (
-		<div>
-		{params.news_type}
+		<div  className="news-detail w-100 mx-[5rem]">
+		<h2 className="flex justify-center p-4 w-full text-2xl">{title}</h2>
+		
+		{
+			text && <p className="flex  justify-center">
+				{text}
+			</p>
+		}
+
+		{url &&  <span className="flex w-full bg-red-100 justify-center"> 
+					More about the news at  
+					<Link className="ml-3 text-blue-500"  href={url}> {url}</Link>
+				</span>
+		}
 		</div>
 		)
 	
